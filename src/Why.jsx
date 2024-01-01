2e2a91fc7904c60b63554dc699acf89daea07b6d
mailtoravi437 <mailtoravi437@gmail.com>
2024-01-02 00:52:40 +0530

    i

--- src/Why.jsx
+++ src/Why.jsx
@@ -1,4 +1,4 @@
-]import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import ArrowWhite from "./assets/ArrowWhite.svg";
 import Expertise from "./assets/Expertise.svg";
 import TailoredSolution from "./assets/TailoredSolution.svg";
